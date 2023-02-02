586e6c9df36fac7467f65c306e9b34ce63c8f9e7
FileBrute <88153990+FileBrute@users.noreply.github.com>
2023-02-02 18:28:48 +0000

    updated the script

--- script/script.sh
+++ script/script.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-site_dir="/path/to/site"
+sitename=$1
+site_dir="/path/to/site/"
 sql_file="/path/to/database.sql"
 
-sitename=$1
-
 # Copy the site to a backup directory
-cp -r $site_dir "$site_dir-backup"
+cp -r $site_dir "$sitename-backup"
 
 # Create a new database and grant privileges to a user
 mysql -u root -p <<EOF
-CREATE DATABASE site_db;
+CREATE DATABASE sitename;
 GRANT ALL PRIVILEGES ON site_db.* TO 'site_user'@'localhost' IDENTIFIED BY 'password';
 EOF
 
 # Load the SQL file into the new database
-mysql -u site_user -p site_db < $sql_file
+mysql -u site_user -p sitename < $sql_file
 
 # Confirm that the database has been loaded successfully
 if [ $? -eq 0 ]; then
